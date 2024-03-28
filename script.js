504c69b5ef434bc9e6b1b171683789c32215e0a9
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2024-03-28 06:12:35 -0500

    added logic

--- script.js
+++ script.js
@@ -11,11 +11,28 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // we add the +1 beacsue we would never actually get 20
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
+    //if there is no guess (0 is falsey value)
+    document.querySelector('.message').textContent = 'No Number!😡'; // if statement checks for value
+  } else if (guess === secretNumber) {
+    //guess same as number
+    document.querySelector('.message').textContent =
+      'You guessed Correctly!!🎉';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent =
+      'You need to guess higher 👆';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent =
+      'You need to guess lower 👇';
   }
 });
