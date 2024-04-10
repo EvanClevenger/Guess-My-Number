74f7c7b69d80382a51fa82a1ca595c196825db31
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2024-04-10 03:50:03 -0500

    edit to the README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Guess-My-Number
 
-This project is to show off my JavaScript and DOM skills
+Hello and welcome to my game! This is a simple game where you can see if you can guess a randomly chosen number between 1 and 20. You can keep attempting to beat your previous record with the built in highscore functionality. When you have guessed the correct number and would like to try again without earasing your highscore, just click the "Again" button to keep going.
+
+I made this small but fun project is to show off my JavaScript and DOM skills. Please feel free to see how I made the game and clone the repo to put your own twist on the app.
+
+Thank you for playing.
--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number! ';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // we add the +1 beacsue we would never actually get 20
 let score = 20;
@@ -23,6 +11,7 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     //if there is no guess (0 is falsey value)
     document.querySelector('.message').textContent = 'No Number!😡'; // if statement checks for value
+    //when guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       'You guessed Correctly!!🎉';
@@ -31,38 +20,52 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highScore) {
+      //highscore logic
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-    //when guess is correct
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        'You need to guess higher 👆';
-      score = score - 1;
-      document.querySelector('.score').textContent = score;
-      //when guess is too low
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game ;(';
-      document.querySelector('.score').textContent = 0;
-    }
-    //////////////////////////////////////////
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        'You need to guess lower 👇';
-      score = score - 1;
-      document.querySelector('.score').textContent = score;
-      //when guess is too high
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game ;(';
-      document.querySelector('.score').textContent = 0;
+    //when guess is wrong
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        document.querySelector('.message').textContent =
+          guess > secretNumber // terninary operator below return a value
+            ? 'You need to guess higher 👆'
+            : 'You need to guess lower 👇'; //if guess > secretNumber then ... or ...;
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost the game ;(';
+        document.querySelector('.score').textContent = 0;
+      }
     }
   }
+  //when guess is too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent =
+  //         'You need to guess higher 👆';
+  //       score = score - 1;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You lost the game ;(';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //      //when guess is too high
+  //   } else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent =
+  //         'You need to guess lower 👇';
+  //       score = score - 1;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You lost the game ;(';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
 });
 
+//resetting all values with again btn
 document.querySelector('.again').addEventListener('click', function () {
-  //resetting all values
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
 
