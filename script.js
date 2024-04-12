fa42d36821794024289066851dc1732167323748
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2024-04-12 06:14:35 -0500

    idk what broke but its fixed now

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1; // we add the +1 beacsue 
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
@@ -13,56 +19,52 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No Number!😡'; // if statement checks for value
     //when guess is correct
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      'You guessed Correctly!!🎉';
+    displayMessage('You guessed Correctly!!🎉');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highScore) {
-      //highscore logic
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
     //when guess is wrong
-    else if (guess !== secretNumber) {
-      if (score > 1) {
-        document.querySelector('.message').textContent =
-          guess > secretNumber // terninary operator below return a value
-            ? 'You need to guess higher 👆'
-            : 'You need to guess lower 👇'; //if guess > secretNumber then ... or ...;
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else {
-        document.querySelector('.message').textContent = 'You lost the game ;(';
-        document.querySelector('.score').textContent = 0;
-      }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      //if guess is higher than secretNumber then ... else ...;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game ;');
+      document.querySelector('.score').textContent = 0;
     }
   }
-  //when guess is too low
-  //   } else if (guess < secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent =
-  //         'You need to guess higher 👆';
-  //       score = score - 1;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'You lost the game ;(';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //      //when guess is too high
-  //   } else if (guess > secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent =
-  //         'You need to guess lower 👇';
-  //       score = score - 1;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'You lost the game ;(';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //   }
 });
+//when guess is too low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent =
+//         'You need to guess higher 👆';
+//       score = score - 1;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You lost the game ;(';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//     //when guess is too high
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent =
+//         'You need to guess lower 👇';
+//       score = score - 1;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You lost the game ;(';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
 
 //resetting all values with again btn
 document.querySelector('.again').addEventListener('click', function () {
